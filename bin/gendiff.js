293a039c23fb17e6db5715172f13f6e3e449b7a7
ANT_SHUR <shurupov.tosha93@gmail.com>
2022-11-10 19:10:39 +0300

    set up eslint into the project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,28 +8,28 @@ const program = new Command();
 
 const sortAndStringify = (array) => {
   const sortedArray = array.sort((a, b) => a[2].toUpperCase().localeCompare(b[2].toUpperCase()));
-	 return `{\n${sortedArray.join(' \n')}\n}`;
+  return `{\n${sortedArray.join(' \n')}\n}`;
 };
 
-export const genDiff = (obj1, obj2) => {
+const genDiff = (obj1, obj2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-	 return (keys.reduce((acc, el) => {
-		  if (Object.hasOwn(obj1, el) && Object.hasOwn(obj2, el)) {
-			   if (obj1[el] === obj2[el]) {
-				    acc.push(`  ${el}: ${obj1[el]}`);
-				  } else {
-					   acc.push(`- ${el}: ${obj1[el]}`);
-					   acc.push(`+ ${el}: ${obj2[el]}`);
-					 }
-			 }
-		  if (Object.hasOwn(obj1, el) && !Object.hasOwn(obj2, el)) {
-			   acc.push(`- ${el}: ${obj1[el]}`);
-			 }
-		  if (!Object.hasOwn(obj1, el) && Object.hasOwn(obj2, el)) {
-			   acc.push(`+ ${el}: ${obj2[el]}`);
-			 }
-		    return acc;
-		  }, []));
+  return (keys.reduce((acc, el) => {
+    if (Object.hasOwn(obj1, el) && Object.hasOwn(obj2, el)) {
+      if (obj1[el] === obj2[el]) {
+        acc.push(`  ${el}: ${obj1[el]}`);
+      } else {
+        acc.push(`- ${el}: ${obj1[el]}`);
+        acc.push(`+ ${el}: ${obj2[el]}`);
+      }
+    }
+    if (Object.hasOwn(obj1, el) && !Object.hasOwn(obj2, el)) {
+      acc.push(`- ${el}: ${obj1[el]}`);
+    }
+    if (!Object.hasOwn(obj1, el) && Object.hasOwn(obj2, el)) {
+      acc.push(`+ ${el}: ${obj2[el]}`);
+    }
+    return acc;
+  }, []));
 };
 
 program
@@ -39,10 +39,10 @@ program
   .argument('<path1>')
   .argument('<path2>')
   .action((path1, path2) => {
-	         const file1 = JSON.parse(fs.readFileSync(path.resolve(path1), 'utf8'));
-	         const file2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
-	         const array = genDiff(file1, file2);
-	         console.log(sortAndStringify(array));
-	         });
+    const file1 = JSON.parse(fs.readFileSync(path.resolve(path1), 'utf8'));
+    const file2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+    const array = genDiff(file1, file2);
+    console.log(sortAndStringify(array));
+  });
 
 program.parse();
