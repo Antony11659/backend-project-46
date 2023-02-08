7c1b47665a6c3302c85efd75ac8d436a77f24be6
ANT_SHUR <shurupov.tosha93@gmail.com>
2023-02-08 21:11:03 +0300

    fix hexlet-tests #2 make format for both files

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import getFormatter from './formatters/index.js';
 
 const genDiff = (file1, file2, formatName = 'stylish') => {
   const buildASTree = getFormatter(formatName);
-  const type = path.extname(file1);
-  const parser = mapping[type];
-  const obj1 = parser(fs.readFileSync(path.resolve(file1), 'utf8'));
-  const obj2 = parser(fs.readFileSync(path.resolve(file2), 'utf8'));
+  const file1Format = path.extname(file1);
+  const file2Format = path.extname(file2);
+  const parser1 = mapping[file1Format];
+  const parser2 = mapping[file2Format];
+  const obj1 = parser1(fs.readFileSync(path.resolve(file1), 'utf8'));
+  const obj2 = parser2(fs.readFileSync(path.resolve(file2), 'utf8'));
   const collection = buildAbstractSyntax(obj1, obj2);
   return buildASTree(collection);
 };
